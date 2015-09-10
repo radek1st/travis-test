78d64f3ec8c5f60236198facaf23c7d8b246fe7f
Radek Ostrowski <dest.hawaii@gmail.com>
2015-09-10 21:07:56 +0100

    travis test

---
+++ src/test/scala/TravisTest.scala
@@ -0,0 +1,14 @@
+import org.scalatest.FunSuite
+
+class TravisTest extends FunSuite {
+
+  test("An empty Set should have size 0") {
+    assert(Set.empty.size == 0)
+  }
+
+  test("Invoking head on an empty Set should produce NoSuchElementException") {
+    intercept[NoSuchElementException] {
+      Set.empty.head
+    }
+  }
+}
